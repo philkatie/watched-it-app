de601e4bb8713c7e3496220f27928597f7f3fa78
philkatie <kaphillips219@gmail.com>
2022-05-23 14:19:31 -0500

    users controller cleanup

--- controllers/users.js
+++ controllers/users.js
@@ -11,22 +11,17 @@ module.exports = {
   login,
 };
 
-function signup(req, res) {
+async function signup(req, res) {
   console.log(req.body, req.file);
 
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
-
-  // FilePath unique name to be saved to our butckt
+  // FilePath unique name to be saved to our bucket
   const filePath = `${uuidv4()}/${req.file.originalname}`;
   const params = {
     Bucket: process.env.BUCKET_NAME,
     Key: filePath,
     Body: req.file.buffer,
   };
-  //your bucket name goes where collectorcat is
-  //////////////////////////////////////////////////////////////////////////////////
+
   s3.upload(params, async function (err, data) {
     console.log(data, "from aws"); // data.Location is our photoUrl that exists on aws
     const user = new User({ ...req.body, photoUrl: data.Location });
@@ -39,7 +34,6 @@ function signup(req, res) {
       res.status(400).json(err);
     }
   });
-  //////////////////////////////////////////////////////////////////////////////////
 }
 
 async function login(req, res) {
@@ -47,7 +41,6 @@ async function login(req, res) {
     const user = await User.findOne({ email: req.body.email });
     console.log(user, " this user in login");
     if (!user) return res.status(401).json({ err: "bad credentials" });
-    // had to update the password from req.body.pw, to req.body password
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (isMatch) {
         const token = createJWT(user);
