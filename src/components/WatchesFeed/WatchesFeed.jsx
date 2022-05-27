7abe62f349c2981dfc93a52d6c22912c9e98fcab
philkatie <kaphillips219@gmail.com>
2022-05-27 00:41:48 -0500

    it would help if I actually added the file that was my breakthrough lol

---
+++ src/components/WatchesFeed/WatchesFeed.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { Card, Dimmer, Segment, Image } from "semantic-ui-react";
+import MovieCard from "../MovieCard/MovieCard";
+import { AppContext } from "../../context/AppContext";
+import { getMovies } from "../../utils/imdbApi";
+
+export default function MovieFeed({movies, watches, numMoviesCol, addToWatched, removeFromWatched, user}) {
+
+    return (
+        <Card.Group itemsPerRow={numMoviesCol} stackable>
+            {watches.map((movie, index) => {
+                return (
+                    <MovieCard
+                        movie={movie}
+                        key={index}
+                        watches={watches}
+                        addToWatched={addToWatched}
+                        removeFromWatched={removeFromWatched}
+                        user={user}
+                    />
+                )
+            })}
+        </Card.Group>
+        );
+    }
